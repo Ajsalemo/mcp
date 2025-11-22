e8598fb523473784371317d868036c94e2924228
Anthony Salemo <ajssalemo@gmail.com>
2025-11-22 00:25:02 -0500

    building out a dotnet mcp application

---
+++ t.sh
@@ -0,0 +1,3 @@
+$ curl -H "Content-Type: application/json" \
+   -d '{"client_id":"eufyhome-app", "client_Secret":"GQCpr9dSp3uQpsOMgJ4xQ", "email":"ajssalemo@gmail.com", "password":"Dudebug1992!"}' \
+   https://home-api.eufylife.com/v1/user/email/login
